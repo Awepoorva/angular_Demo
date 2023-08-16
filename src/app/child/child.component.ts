22a03e494e84fb5ec89093b41c99baf3fdeaacb5
Awepoorva <apurvac329@gmail.com>
2023-08-16 10:09:59 +0530

    new

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'child',
@@ -8,4 +8,11 @@ import { Component, Input } from '@angular/core';
 export class ChildComponent {
    @Input('parentData')
    pd:any;
+
+   @Output()
+   public childValue = new EventEmitter();
+
+   emitValue() {
+    this.childValue.emit('Hi Priya');
+  }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ParentComponent {
   myData = 'Happy Independence day';
+  msgFromChild = '';
 }
