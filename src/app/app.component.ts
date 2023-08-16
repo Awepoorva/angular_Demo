2ee22d2b50ed99a4d0b59af465e32cc827e28257
Awepoorva <apurvac329@gmail.com>
2023-08-16 10:01:15 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-project';
+  name = "Alia";
+  myid = 1234;
+  isDisabled = true;
+  successClass = 'success';
+  errorClass = 'error';
+  hasError = false;
+  msgClass = {
+     success: !this.hasError,
+     error: this.hasError
+  };
+
+  myprop = "yellow";
+  mystyle = {
+    color: "brown",
+    fontStyle : "italic",
+    backgroundColor : "pink"
+  };
+
+  sayHi(){
+    console.log("Hello World!");
+  }
+
+  email = "abc@gmail.com";
+  onKeyUpp(em: any){
+    console.log(em);
+    console.log(this.email);
+  }
+
+  displayName = true;
+  color = 'blue';
+
+  colors = ['pink','yellow','blue'];
+
+  today = Date.now();
+  emp = {
+     id : 10,
+     ename : "hjdqcbj"
+  };
+
+  price:number = 1234.78;
 }
