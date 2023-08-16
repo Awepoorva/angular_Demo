776d2f945fa119ea6454ebfe73b40b7bff0ac489
Awepoorva <apurvac329@gmail.com>
2023-08-16 10:02:02 +0530

    Second commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+   @Input('parentData')
+   pd:any;
+}
